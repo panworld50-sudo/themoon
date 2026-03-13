93185a9412e73faf1135b87cf907927f64c860eb
panworld50-sudo <panworld50@gmail.com>
2026-03-13 23:56:12 +0530

    Create TrendingSlider.tsx

---
+++ components/TrendingSlider.tsx
@@ -0,0 +1,16 @@
+export default function TrendingSlider(){
+
+return(
+
+<div className="grid md:grid-cols-4 gap-4 mt-6">
+
+<div className="bg-gray-800 p-4 rounded">AI Future</div>
+<div className="bg-gray-800 p-4 rounded">Markets</div>
+<div className="bg-gray-800 p-4 rounded">Climate</div>
+<div className="bg-gray-800 p-4 rounded">Space</div>
+
+</div>
+
+)
+
+}
