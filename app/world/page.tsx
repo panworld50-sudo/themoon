6711fa1018c4c75edc00559153e5192dc1a9dab5
panworld50-sudo <panworld50@gmail.com>
2026-03-13 23:47:50 +0530

    Create page.tsx

---
+++ app/world/page.tsx
@@ -0,0 +1,17 @@
+export default function World(){
+
+return(
+
+<div className="max-w-6xl mx-auto p-6">
+
+<h1 className="text-3xl font-bold mb-6">
+World News
+</h1>
+
+<p>Global breaking stories appear here.</p>
+
+</div>
+
+)
+
+}
