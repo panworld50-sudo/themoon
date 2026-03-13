edea82b01166ff679bfeebf588a0e272aca60288
panworld50-sudo <panworld50@gmail.com>
2026-03-13 23:53:49 +0530

    Create BreakingTicker.tsx

---
+++ components/BreakingTicker.tsx
@@ -0,0 +1,17 @@
+export default function BreakingTicker(){
+
+return(
+
+<div className="bg-red-600 p-2">
+
+<marquee>
+
+Breaking • Global markets surge • AI revolution expanding • Space mission success
+
+</marquee>
+
+</div>
+
+)
+
+}
